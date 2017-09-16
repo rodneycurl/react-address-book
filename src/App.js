17e1bcf2027a240c3db9f6e1568789271b4453ea
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-09-16 12:06:12 -0400

    adding new contacts to the list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react'
 
 function App() {
-  const contacts = {
+  const contacts = [
+    {
     name: "Mark Zuckerberg",
     phonenumber: "555-555-5555",
     email: "mark@facebook.com",
     address: "1 Hacker Way, Menlo Park, 94025",
-    }
+    },
+    {
+    name: "Dustin Moskovitz",
+    phonenumber: "555-555-5555",
+    email: "dustin@facebook.com",
+    address: "1 Hacker Way, Menlo Park, 94025",
+    },
+    {
+    name: "Eduardo Saverin",
+    phonenumber: "555-555-5555",
+    email: "eduardo@facebook.com",
+    address: "1 Hacker Way, Menlo Park, 94025",
+    },
+    {
+    name: "Andrew McCollum",
+    phonenumber: "555-555-5555",
+    email: "andrew@facebook.com",
+    address: "1 Hacker Way, Menlo Park, 94025",
+    },
+    {
+    name: "Chris Hughes",
+    phonenumber: "555-555-5555",
+    email: "chris@facebook.com",
+    address: "1 Hacker Way, Menlo Park, 94025",
+    },
+  ]
 
   return (
     <section>
       <h1 style={{marginLeft: 20 +'em'}}>Address Book</h1>
+      { contacts.map(contact => (
       <div>
-        <header style={{marginLeft: 30 +'em'}}>{ contacts.name }</header>
         <p style={{marginLeft: 30 +'em'}}>
-          Phone Number: { contacts.phonenumber }
-          <br />
-          Email: { contacts.email }
-          <br />
-          Address: { contacts.address }
+          <text style={{fontWeight: 'bold'}}>Contact Name: </text> { contact.name } <br />
+          <text style={{fontWeight: 'bold'}}>Phone Number: </text> { contact.phonenumber } <br />
+          <text style={{fontWeight: 'bold'}}>E-mail: </text>{ contact.email } <br />
+          <text style={{fontWeight: 'bold'}}>Address: </text>{ contact.address } <br />
         </p>
       </div>
+    ))}
     </section>
   )
 }
